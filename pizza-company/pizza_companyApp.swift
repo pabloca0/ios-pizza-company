c046db740f1b193fdcec72d0706f57dc8b8c05e1
Fernando Salom <fer@rudo.es>
2024-01-17 09:22:12 +0100

    Add initial project

---
+++ pizza-company/pizza_companyApp.swift
@@ -0,0 +1,32 @@
+//
+//  pizza_companyApp.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 16/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct pizza_companyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
