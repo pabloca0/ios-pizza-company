e11ddb307cf7be8acf1387876fb35e5e1adfe7ed
Fernando Salom <fer@rudo.es>
2024-01-19 22:13:12 +0100

    Add entities

---
+++ pizza-company/Entities/Ingredient.swift
@@ -0,0 +1,12 @@
+//
+//  Ingredient.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 19/1/24.
+//
+
+import Foundation
+
+struct Ingredient {
+    var name: String
+}
---
+++ pizza-company/Entities/Stock.swift
@@ -0,0 +1,13 @@
+//
+//  Stock.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 19/1/24.
+//
+
+import Foundation
+
+struct Stock {
+    var ingredient: Ingredient
+    var quantity: Int
+}
