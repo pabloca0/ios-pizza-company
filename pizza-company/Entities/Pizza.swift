75c30ff59f4f0e435e01f244b63fb5be0d2a9f4f
Fernando Salom <fer@rudo.es>
2024-01-21 19:20:21 +0100

    Add pizza entity

---
+++ pizza-company/Entities/Pizza.swift
@@ -0,0 +1,13 @@
+//
+//  Pizza.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 20/1/24.
+//
+
+import Foundation
+
+struct Pizza {
+    var ingredients: [Ingredient]
+    var price: Double
+}
