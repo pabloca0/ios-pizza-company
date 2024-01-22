9df9362962d72534de2157d9c8f2364f71c60987
Fernando Salom <fer@rudo.es>
2024-01-22 10:17:02 +0100

    Add stock and order

---
+++ pizza-company/Entities/Order.swift
@@ -0,0 +1,13 @@
+//
+//  Order.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 20/1/24.
+//
+
+import Foundation
+
+struct Order {
+    var pizzas: [Pizza]
+    var total: Float = 0.0
+}
--- pizza-company/Entities/Stock.swift
+++ pizza-company/Entities/Stock.swift
@@ -10,4 +10,9 @@ import Foundation
 class Stock {
     var ingredient: Ingredient
     var quantity: Int
+
+    init(ingredient: Ingredient, quantity: Int) {
+        self.ingredient = ingredient
+        self.quantity = quantity
+    }
 }
