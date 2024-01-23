dcfeb89ed8d34d7bce8f04a7a223474a2a489fa7
Fernando Salom <fer@rudo.es>
2024-01-23 11:33:12 +0100

    Fix views and start logic

---
+++ pizza-company/App.swift
@@ -0,0 +1,18 @@
+//
+//  pizza_companyApp.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 16/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantView()
+        }
+    }
+}
