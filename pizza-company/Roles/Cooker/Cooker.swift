2cdec8d34822c7ad4b52eac6a1f8c85d51f2897e
Fernando Salom <fer@rudo.es>
2024-01-23 11:35:37 +0100

    Add cooker pending implementation

---
+++ pizza-company/Roles/Cooker/Cooker.swift
@@ -0,0 +1,24 @@
+//
+//  Cooker.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 22/1/24.
+//
+
+import Foundation
+
+class Cooker: CookerProtocol {
+    var kitchenAssistant: KitchenAssistantProtocol
+
+    init(kitchenAssistant: KitchenAssistantProtocol) {
+        self.kitchenAssistant = kitchenAssistant
+    }
+
+    private func takeAll(ingredients: [Ingredient]) -> [Ingredient] {
+        // Úsalo si crees que lo necesitas. No es obligatorio
+    }
+
+    func cook(this pizza: Pizza) -> FoodStatus {
+        // Una pizza esta cocinada cuando tienes todos los ingredientes para poderla hacer
+    }
+}
