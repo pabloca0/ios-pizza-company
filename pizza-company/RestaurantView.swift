f186acbf5bc06516ea0d3646f851a115eebf9ac1
Fernando Salom <fer@rudo.es>
2024-01-23 11:34:36 +0100

    Add main dependencies

---
+++ pizza-company/RestaurantView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  pizza-company
+//
+//  Created by Fernando Salom Carratala on 16/1/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct RestaurantView: View {
+
+    var body: some View {
+        var wareHouse = WareHouse(stocks: stocks)
+        var kitchenAssistant = KitchenAssistant(wareHouse: wareHouse)
+        var cooker = Cooker(kitchenAssistant: kitchenAssistant)
+        var clerk = Clerk(cooker: cooker)
+        var restaurant = Restaurant(clients: clients, clerk: clerk)
+        HStack {
+            Text("Francesco`s pizza")
+        }.onAppear {
+            restaurant.open()
+        }
+    }
+}
+
+#Preview {
+    RestaurantView()
+}
