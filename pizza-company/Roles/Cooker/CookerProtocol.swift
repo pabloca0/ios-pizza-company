ceddd13659326a5fdc1f651fe34df723ae62ecbc
Fernando Salom <fer@rudo.es>
2024-01-26 21:28:45 +0100

    Add kitchen error

--- pizza-company/Roles/Cooker/CookerProtocol.swift
+++ pizza-company/Roles/Cooker/CookerProtocol.swift
@@ -4,6 +4,11 @@ enum FoodStatus {
     case done
     case missingIngredients
 }
+
+enum KitchenError: Error {
+    case notEnoughIngredients
+}
+
 protocol CookerProtocol {
     func cook(this pizza: Pizza) -> FoodStatus
 }
